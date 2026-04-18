8b97daa6c6dda3c808d55cfe881a590e3800b82f
ayad alharbi <ayadalharbi232@gmail.com>
2026-04-18 03:03:04 +0100

    finished hw

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
 // App.js
 import React from 'react';
-import Tweet from './Tweet';
+import Tweet from './tweet';
 import { tweetsData } from './data';
 
 /* Do not write your tweet component here, write it in the tweet.tsx file */
+
 
 function App() {
   return (
     <div className="app">
       <h1>Twitter Clone</h1>
       <div className="feed">
-        {/* Here you will use the data from the 'tweetsData' variable and populate every tweet
-          that you received from the 'backend' (data.ts).
-         */}
+        {
+          
+            tweetsData.map(tweet => (
+              <Tweet key = {1} {...tweet} />
+            ))
+          }
       </div>
     </div>
   );
--- src/tweet.tsx
+++ src/tweet.tsx
@@ -1,6 +1,35 @@
 // Tweet.js
 import React, { useState } from 'react';
 
+type TweetProps = {
+    id: number;
+    username: string;
+    content: string;
+    likes: number;
+    timestamp: string;
+};
+
+function Tweet({ username, content, likes: initialLikes, timestamp }: TweetProps) {
+    const [likes, setLikes] = useState(initialLikes)
+    const [liked, setLiked] = useState(false)
+    const [heart, setHeart] = useState("🤍")
+    return (
+        <div className = "tweet">
+            <h3>@{username}</h3>
+            <p>{content}</p>
+            <small> {timestamp}</small> 
+            <br />
+            <button onClick={
+                () => {
+                    liked ? setLikes(likes - 1) : setLikes(likes + 1)
+                    liked ? setHeart("🤍") : setHeart("❤️")
+                    setLiked(!liked)
+                }}>
+            {heart} {likes} likes
+            </button>
+        </div>
+    )
+}
 /* 
 Create a new componnet called 'Tweet' make sure to pass down all the props you will need.
 You will also need to use 'useState' for two variables. Then you want to create an event
@@ -13,4 +42,4 @@ Theres a 'tweet' class you can use to style your tweet.
 
 
 
-// export default Tweet;
+export default Tweet;
